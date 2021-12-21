4d4096978546f2f25f6076e407cd30d5405df9c0
Timowest12 <timowester@gmail.com>
2021-12-21 22:22:48 +0100

    fix update issue

--- src/components/RocketCard.js
+++ src/components/RocketCard.js
@@ -3,8 +3,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import { reserveRocket } from '../actions'
 
 const RocketCard = (data) => {
-    const [isReserved,setIsreserved] = useState(false)
+    const [isReserved,setIsreserved] = useState(data.data.reserved)
     const reserved = useSelector(state => state)
+
     const dispatch = useDispatch()
     //alert(data.data.rocket_name)
     const reserveRocket = () => {
@@ -28,12 +29,15 @@ const RocketCard = (data) => {
             </div>
             <div className="rightcard">
             <h5 className="rockettitle">{data.data.rocket_name}</h5>
-            <p className="rocketdescription">{data.data.description}</p>
+            <p className="rocketdescription">
+                {data.data.reserved ? <span className="reservedbadge">reserved</span>: <span></span>}
+                
+                {data.data.description}</p>
             {data.data.reserved ? 
             <button onClick={cancelReservation} className="cancelReservation">Cancel reservation</button> 
             :
             <button onClick={reserveRocket} className="reservebutton">Reserve Rocket</button>}
-            <h3>{isReserved}</h3>
+           
             
             
             
