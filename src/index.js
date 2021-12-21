559c4782f5a172ae87b7a52a991bbd33dfbbe90e
Timowest12 <timowester@gmail.com>
2021-12-21 20:44:27 +0100

    Add provider wrapper

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import MyProfile from './pages/MyProfile';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk'
+import rocketsReducer from './reducer';
+import logger from 'redux-logger';
+
+
+const store = createStore(rocketsReducer,applyMiddleware(thunk,logger))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
-
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
