66fa49269527d558bdd98216e533a6182aa66c21
Timowest12 <timowester@gmail.com>
2021-12-22 11:03:08 +0100

    Add profilepage rendering

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -2,31 +2,24 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const MyProfile = () => {
-  const rockets = useSelector(state => state)
-  console.log(rockets.length)
+  const rockets = useSelector(state => state.rockets)
+  const missions = useSelector(state => state.missions)
+  console.log(missions.missions.length)
   return (
     <div className="profilePage">
     <div className="myMissions">
         <h2>My Missions</h2>
       <table>
-  <thead>
-    <tr>
-      <th>Telstar</th>
-    </tr>
-     <tr>
-      <th>SES</th>
-    </tr>
-     <tr>
-      <th>AsiaSat</th>
-    </tr>
-     <tr>
-      <th>ABS</th>
-    </tr>
-  </thead>
-  <tbody>
-  </tbody>
-  <tfoot>
-  </tfoot>
+ 
+  {missions.missions.map(item => 
+     
+     
+item.reserved ? <tr><th>{item.name}</th></tr>:null
+   
+    )}
+    
+  
+ 
 </table>
     </div>
       
@@ -36,12 +29,11 @@ const MyProfile = () => {
 </h2>
 <table>
   <thead>
-    {rockets.map((item) => 
-      <tr>
-      {item.reserved ? 
-      <th>{item.rocket_name}</th>
-       : null}
-    </tr>
+    {rockets.map(item => 
+      
+      item.reserved ? 
+        <tr><th>{item.rocket_name}</th></tr>
+       : null
     )}
      
   </thead>
