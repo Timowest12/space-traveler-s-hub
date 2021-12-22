5c91902dc886f992ab7bb3583f804726eb80d7ea
Timowest12 <timowester@gmail.com>
2021-12-22 18:11:55 +0100

    fix issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import App from './App';
 import Missions from './components/missions/missions';
 import RocketCard from './components/RocketCard';
-import store from './pages/configureStore'
+import store from './pages/configureStore';
 import MyProfile from './pages/MyProfile';
 
 test('renders learn react link', () => {
@@ -22,7 +21,11 @@ test('test if rockets succesfully executes', () => {
   expect(linkElement).toBeInTheDocument();
 });
 test('see if rocketcard renders succesfully', () => {
-  const mockdata = {data:{id:23,flickr_images:'logo192.png',rocket_name:'falconHeavy',reserved:'true',description:"falcan X heavy launcher"}}
+  const mockdata = {
+    data: {
+      id: 23, flickr_images: 'logo192.png', rocket_name: 'falconHeavy', reserved: 'true', description: 'falcan X heavy launcher',
+    },
+  };
   render(<Provider store={store}><RocketCard data={mockdata} /></Provider>);
   const linkElement = screen.getByText(/rocket/i);
   expect(linkElement).toBeInTheDocument();
